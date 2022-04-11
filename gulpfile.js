ee590e8e9afea128f95b10b38bc952f0c3763962
dimpulsha <dimpulsha@gmail.com>
2022-04-11 14:44:53 +0500

    linter

--- gulpfile.js
+++ gulpfile.js
@@ -113,8 +113,8 @@ const refresh = (done) => {
   done();
 };
 
-// const build = gulp.series(clean, svgo, copy, css, sprite, js);
-const build = gulp.series(clean, copy, css, sprite, js);
+const build = gulp.series(clean, svgo, copy, css, sprite, js);
+// const build = gulp.series(clean, copy, css, sprite, js);
 
 const start = gulp.series(build, syncServer);
 
